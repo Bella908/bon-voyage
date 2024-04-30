66ef96915070e8a3fb6f5c147f8b54463d8c200f
bella908 <mumtahina efa8@gmail.com>
2024-04-30 12:16:37 +0600

    update mylist

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1581552456143-0cd325c6a20d?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1588387695597-87bfcf4cd1bf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1554205163-beedc3a3e9fd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1596622678000-e55d2266ed7e?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Show List/ShowList.jsx
+++ src/pages/Show List/ShowList.jsx
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 const ShowList = () => {
     const { user } = useContext(AuthContext);
     const [item, setItem] = useState([]);
-    const [control ,setControl] = useState(false);
+    const [control, setControl] = useState(false);
 
 
     useEffect(() => {
@@ -15,52 +15,90 @@ const ShowList = () => {
             .then(data => {
                 setItem(data);
             });
-    }, [user , control]);
+    }, [user, control]);
 
     const handleDelete = (id) => {
-      // Display SweetAlert confirmation dialog
-      Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
-          if (result.isConfirmed) {
-              // If confirmed, proceed with deletion
-              fetch(`http://localhost:5000/showList/delete/${id}`, {
-                  method: "DELETE",
-              })
-              .then(res => res.json())
-              .then(data => {
-                  if(data.deletedCount > 0) {
-                      setControl(!control);
-                      // Show success message after deletion
-                      Swal.fire({
-                          title: "Deleted!",
-                          text: "Your file has been deleted.",
-                          icon: "success"
-                      });
-                  }
-              });
-          }
-      });
-  };
+        // Display SweetAlert confirmation dialog
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // If confirmed, proceed with deletion
+                fetch(`http://localhost:5000/showList/delete/${id}`, {
+                    method: "DELETE",
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            setControl(!control);
+                            // Show success message after deletion
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    });
+            }
+        });
+    };
     return (
         <div>
-            {
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Contry Name</th>
+                            <th>Tourists spot</th>
+                            <th>Location</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            item?.map(p => <tr key={p._id}>
+                                <th>1</th>
+                                <td>{p.country_Name}</td>
+                                <td>{p.tourists_spot_name}</td>
+
+                                <td>{p.location}</td>
+                                <td>
+                                    <div className='my-2'>
+                                        <Link to={`/showList/update/${p._id}`}>
+
+                                            <button className="btn bg-orange-300 text-white ">Update</button>
+                                        </Link>
+                                        </div>
+
+                                        <button onClick={() => handleDelete(p._id)} className="btn bg-red-500 text-white">Delete</button>
+                                    
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+            {/* {
                 item?.map(p => (
                     <div key={p._id}>
                         <h1>name : {p.location}</h1>
+
                         <Link to={`/showList/update/${p._id}`}>
                             <button>Update</button>
                         </Link>
                         <button onClick={() => handleDelete(p._id)}>Delete</button>
                     </div>
                 ))
-            }
+            } */}
         </div>
     );
 };
